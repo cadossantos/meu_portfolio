ad73dadfa66841634a085279a855287de662c40f
Cláudio dos Santos <cadossantos@proton.me>
2025-05-10 11:01:41 -0300

    primeiro commit

---
+++ portfolio/dados.py
@@ -0,0 +1,17 @@
+habilidades = {
+    'habilidade1': {
+        'nome': 'Python'
+    },
+    'habilidade2': {
+        'nome': 'Django'
+    },
+    'habilidade3': {
+        'nome': 'Streamlit'
+    },
+    'habilidade4': {
+        'nome': 'LangGraph'
+    },
+    'habilidade5': {
+        'nome': 'Pandas'
+    }
+}
